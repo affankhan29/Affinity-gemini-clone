d10eb9657089392b62247f5ef1aee7f5116de9dc
affankhan29 <163327221+affankhan29@users.noreply.github.com>
2024-08-13 23:03:02 +0500

    added localstorage and improved scrolling ,response.

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 const typingForm = document.querySelector('.typing-form');
 const chatList = document.querySelector('.chat-list');
+const toggleIcon= document.querySelector('#toggleIcon');
+const resetIcon= document.querySelector('#resetIcon');
 let userMessage = null;
 const APIKey="AIzaSyBZXmZNECCSkUYQhGlLOPMtyqLwfDLTjO0" ;
 const APIUrl= `https://generativelanguage.googleapis.com/v1/models/gemini-pro:generateContent?key=${APIKey}`;
+function loadLocalStorage(){
+  const savedChat = localStorage.getItem('chatlist');
+  chatList.innerHTML=savedChat ||  '';
+  chatList.scrollTo(0,chatList.scrollHeight);
+
+}
+loadLocalStorage();
 function createMessageElement(content,...classes){
     const div = document.createElement('div');
     div.classList.add('message',...classes);
@@ -10,15 +19,22 @@ function createMessageElement(content,...classes){
     return div ;
 }
 
-function showTypingEffect(text, textDiv){
+function showTypingEffect(text, textDiv , incomingDiv){
   const words = text.split(' ');
   let i = 0;
   const timer = setInterval(() => {
     textDiv.innerText += (i===0 ? "" : " ")+words[i++];
+    chatList.scrollTo(0,chatList.scrollHeight);
+    incomingDiv.querySelector('.icon').classList.add('hide');
     if(i === words.length){
         clearInterval(timer);
+        incomingDiv.querySelector('.icon').classList.remove('hide');
+        localStorage.setItem('chatlist',chatList.innerHTML);
+      
+       
     }
   },75);
+
 }
  async function generateAPIResponse(incomingDiv){
       const textDiv = incomingDiv.querySelector('.text');
@@ -36,9 +52,9 @@ function showTypingEffect(text, textDiv){
             })
         });
         const data = await response.json();
-        const responseText = data?.candidates[0].content.parts[0].text;
+        const responseText = data?.candidates[0].content.parts[0].text.replace(/\*\*(.*?)\*\*/g, '$1');
         // console.log(responseText);
-        showTypingEffect(responseText,textDiv);
+        showTypingEffect(responseText,textDiv, incomingDiv);
         incomingDiv.classList.remove('loading');
 
       }catch(error){
@@ -56,9 +72,10 @@ function showLoadingAnimation(){
                     <div class="loading-bar"></div>
                    </div>
                  </div>
-                  <span class="icon material-symbols-rounded">content_copy</span>`;
+                  <span onclick="copyResponseText(this);" class="icon material-symbols-rounded">content_copy</span>`;
    const incomingDiv =  createMessageElement(html,'incoming', 'loading');
     chatList.appendChild(incomingDiv);
+    chatList.scrollTo(0,chatList.scrollHeight);
     generateAPIResponse(incomingDiv);
 }
 function handleOutgoingChat(){
@@ -71,6 +88,7 @@ function handleOutgoingChat(){
    const outgoingDiv =  createMessageElement(html,'outgoing');
     chatList.appendChild(outgoingDiv);
     typingForm.reset();
+    chatList.scrollTo(0,chatList.scrollHeight);
     setTimeout(() => {
         showLoadingAnimation();
     }, 500);
@@ -83,3 +101,24 @@ typingForm.addEventListener('submit', (e) => {
 
     
 });
+ function copyResponseText(copyElement){
+    const text = copyElement.parentElement.querySelector('.text').textContent;
+    navigator.clipboard.writeText(text);
+    copyElement.textContent = "done";
+    setTimeout(() => {
+        copyElement.textContent = "content_copy";
+    }, 2000);
+   }
+
+   
+function toggleTheme(toggleIcon){
+  const  isLightMode = document.body.classList.toggle('light_mode');
+   toggleIcon.innerText= isLightMode ? 'dark_mode' : 'light_mode';
+} 
+ function resetLocalStorage(){
+  resetIcon.addEventListener('click', ()=>{
+    localStorage.clear();
+    chatList.innerHTML= '';
+  });
+ }
+resetLocalStorage();
